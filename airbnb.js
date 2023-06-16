4c188c7500009c6244e4b5f9889f109ca70cee38
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-06-16 03:05:45 +0100

    working on functions guidelines

--- airbnb.js
+++ airbnb.js
@@ -368,3 +368,70 @@ function processInput(input) {
 
 // the caller selects only the data they need
 const { left, top } = processInput(input);
+
+
+//======STRINGS=========
+// Use single quotes '' for strings.
+// bad
+const name = "Capt. Janeway";
+
+// bad - template literals should contain interpolation or newlines
+const name = `Capt. Janeway`;
+
+// good
+const name = 'Capt. Janeway';
+
+
+// Strings that cause the line to go over 100 characters should not be written across multiple
+// lines using string concatenation
+// Why? Broken strings are painful to work with and make code less searchable
+// bad
+const errorMessage = 'This is a super long error that ws thrown because \
+of Batman. When you stop to think about how Batman had amything to do \
+with this, you could get nowhere \
+fast.';
+
+// bad
+const errorMessage = 'This is a super long error that was thrown becasue' +
+    'of Batman. When you stop to think about how Batman had anything to do ' +
+    ' with this, you would get nowhere fast.';
+
+// good
+const errorMessage = 'This is a super long error that was thrown because of Batman. When you stop to think about how Batman had anything to do with this, you would get nowhere fast.';
+
+
+// When programmatically building up strings, use templat strings instead of concatenation.
+// Why? Template strings give you a readable, concise syntax with proper newlines and string interpolation features.
+// bad
+function sayHi(name) {
+    return 'How are you, ' + name + '?';
+}
+
+// bad
+function sayHi(name) {
+    return ['How are you, ', name, '?'].join();
+}
+
+// bad
+function sayHi(name) {
+    return `How are you, ${ name }?`;
+}
+
+// good
+function sayHi(name) {
+    return `How are you, ${name}?`;
+}
+
+
+// Never use eval() on a string, it opens too many vulnerabilities.
+
+// Do not unnecessarily escape characters in strings.
+// Why? Backslashes harm readability, thus they should only be present when necessary.
+// bad
+const foo = `\`this\` \i\s \"quoted\"`;
+
+// good
+const foo = '\'this\' is "quoted"';
+const foo = `my name is '${name}'`;
+
+
---
+++ functions.js
@@ -0,0 +1,161 @@
+
+//=======FUNCTIONS===========
+// Use named function expressions instead of function declarations.
+// Why? Function declarations are hoisted, which means that it's easy - too easy - to reference
+// the function before it is defined in the file. This harms readability and maintainability. 
+// If you find that a function's definition is large or complex enough that it is interfering 
+// with understanding the rest of the file, then perhaps it's time to extract it to its own module!
+// Don't forget to explicityly name the expression, regardless of whether or not the name is inferred
+// from the containing variable (which is often the case in modern browsers or when using compilers such as Babel).
+// This eliminates any assumptions made about the Error's call stack.
+// bad 
+function foo() {
+    // ...
+}
+
+// bad
+const foo = function() {
+    // ...
+}
+
+// good
+// lexical name distinguished from the variable-reference invocation(s)
+const short = function longUniqueMoreDescriptiveLexicalFoo() {
+    // ...
+}
+
+
+// Wrap immediately invoked function expressions in parenthesis.
+// Why? An immediately invoked function expression is a single unit - wrapping both it, and 
+// its invocation parens, in parens, cleanly expresses this. Note that in a world with modules
+// everywhere, you almost never need an IIFE.
+// immediately-invoked function expression (IIFE)
+(function() {
+    console.log('Welcome to the Internet. Please follow me.');
+}());
+
+
+// Never declare a function in a non-function block(if, while, etc). Assign the function to 
+// a variable instead. Browsers will alow you to do it, but they all interpret it differently, 
+// which is bad news bears.
+
+// Note: ECMA-262 defines a block as a list of statements. A function declaration is not a statement.
+// bad
+if (currentUser) {
+    function test() {
+        console.log('Nope.');
+    }
+}
+
+// good
+let test;
+if (currentUser) {
+    test = () => {
+        console.log('Yup.');
+    };
+}
+
+// Never name a parameter arguments. This will take precedence over the arguments object that is
+// given to every function scope.
+// bad
+function foo(name, options, arguments) {
+    // ...
+}
+
+// good
+function foo(name, options, args) {
+    // ...
+}
+
+// Never use arguments, opt to use rest syntax ... instead.
+// Why? ... is explicit about which arguments you want pulled. Plus, rest arguments are a 
+// real Array, and not merely Array-like like arguments.
+// bad
+function concatenateAll() {
+    const args = Array.prototype.slice.call(arguments);
+    return args.join('');
+}
+
+// good
+function concatenateAll(...args) {
+    return args.join('');
+}
+
+
+// Use default parameter syntax rather than mutating function arguments.
+// really bad
+function handleThings(opts) {
+    // No! We shouldn't mutate function arguments.
+    // Double bad; if opts is falsy it'll be set to an object which may
+    // be what you want but it can introduce subtle bugs.
+    opts = opts || {};
+    // ...
+}
+
+// still bad
+function handleThings(opts) {
+    if (opts === void 0) {
+        opts = {};
+    }
+    // ...
+}
+
+// good
+function handleThings(opts = {}) {
+    // ...
+}
+
+
+// Avoid side effects with default parameters.
+// Why? They are confusing to reason about.
+var b = 1;
+// bad
+function count(a = b++) {
+    console.log(a);
+}
+count(); // 1
+count(); // 2
+count(3); // 3
+count(); // 3
+
+// Always put default parameter last
+// bad
+function handleThings(opts = {}, name) {
+    // ...
+}
+
+// good
+function handleThings(name, opts = {}) {
+    // ...
+}
+
+// Never use the function constructor to create a new function.
+// Why? Creating a function in this way evaluates a string similarly to eval(), which opens vulnerabilities.
+// bad
+var add = new Function('a', 'b', 'return a + b');
+
+// still bad
+var subtract = Function('a', 'b', 'return a - b');
+
+
+// Spacing in a function signature.
+// Why? Consistnecy is good, and you shouldn't have to add or remove a space when adding or removing a name.
+// bad
+const f = function() {};
+const g = function(){};
+const h = function(){};
+
+// good
+const x = function () {};
+const y = function a() {};
+
+
+// Never mutate parameters.
+// Why? Manipulating objects passed in as parameters can cause unwanted variable side effects
+// in the original caller.
+// bad
+function f1(obj) {
+    obj.key = 1;
+}
+
+// good
